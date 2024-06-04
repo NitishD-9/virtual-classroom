6975ca7023405a85066b6e53f76eabfe1290a4e9
Nitish D <75166166+NitishD-9@users.noreply.github.com>
2024-06-04 23:46:14 +0530

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "fa9e82bfad66c0835b024e0e3a10bacb992f71a062385264a3db2ff9084f0907",
+  mongoUri: process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject'
+}
+
+export default config
